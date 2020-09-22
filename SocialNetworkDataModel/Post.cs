01215f50841cebea271ab15e400a2d942eed3bd6
Chlorine-trifluoride <67011284+Chlorine-trifluoride@users.noreply.github.com>
2020-09-22 17:15:31 +0300

    DataModel

---
+++ SocialNetworkDataModel/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SocialNetworkDataModel
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+
+        [Required] // Not necessariy but should return a more clear error message
+        [MinLength(3)]
+        [MaxLength(300)]
+        public string Content { get; set; }
+    }
+}
---
+++ SocialNetworkDataModel/Profile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetworkDataModel
+{
+    public class Profile
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+        public List<int> FriendIDs { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
